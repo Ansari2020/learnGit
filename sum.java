b3d622784fe924efe454339bd3edfbfed5426a5d
Danish Ansari <danishdeveloper2018@gmail.com>
2024-05-28 14:44:54 +0530

    intial commit of sum

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
-class sum{
+public class sum {
+
     public static void main(String[] args) {
-        
         int a=10;
-        int b=20;
-        int sum=a+b;
-        System.out.println("sum of two number is "+sum);
+        int b=30;
+        System.out.println(a+b);
     }
+    
 }
